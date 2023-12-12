b71a82d202db81878aa7285ae54cb378fcc83b41
kaustubhr2001@gmail.com <87487138+kaustubhraut2001@users.noreply.github.com>
2023-12-12 14:08:55 +0530

    some more css fixes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-scroll";
-import Logo from "../../public/PortfolioLogo.png";
+import {Link} from "react-scroll";
+import Logo from "../../public/PortfolioLogo.png"
 import darkicon from "../../public/icons8-dark-mode-50.png";
 import lighticon from "../../public/icons8-light-mode-78.png";
 import { useDispatch } from "react-redux";
 import { toggleDarkMode } from "../../Redux/darkmodeslice";
-import { FaBars, FaTimes } from "react-icons/fa";
 
+import { FaBars, FaTimes } from "react-icons/fa";
 const NavBar = () => {
   const dispatch = useDispatch();
-  const [nav, setNav] = useState(false);
+  const [nav, setnav] = useState(false);
   const [icon, setIcon] = useState(false);
-
   const toggle = () => {
     dispatch(toggleDarkMode(icon)); // Pass the current state of icon to toggleDarkMode
     setIcon(!icon);
   };
-
   const links = [
     {
       id: 1,
@@ -33,37 +31,51 @@ const NavBar = () => {
     },
     {
       id: 4,
-      link: "experience",
+      link: "experiance",
     },
     {
       id: 5,
       link: "contact",
     },
   ];
-
   return (
-    <div className={`flex justify-between items-center w-full h-20 px-4 ${nav? "text-white bg-black" : "text-white bg-gray-300 fixed opacity-50"} `}>
-      <div className={`w-24 md:w-auto  bg-gray-300 `}>
-        <img src={Logo} alt="logo" className="w-full h-auto" />
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-gray-300 bg-opacity-50 fixed">
+      <div >
+
+        <img src={Logo} alt="logo" />
       </div>
-      <ul className="hidden md:flex space-x-4">
-        {links.map((items) => (
+      <ul className="hidden md:flex">
+        {links.map((itmes) => (
           <li
-            key={items.id}
-            className={`cursor-pointer capitalize font-medium text-black  hover:scale-105 duration-200`}
+            key={itmes.id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-200 hover:scale-105 duration-200"
           >
-            <Link to={items.link} smooth duration={500}>{items.link}</Link>
+            <Link to={itmes.link} smooth duration={500}>{itmes.link}</Link>
           </li>
         ))}
       </ul>
 
       <div
-        onClick={() => setNav(!nav)}
+        onClick={() => setnav(!nav)}
         className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
-      <div className="w-10 h-10 bg-gray-300 flex items-center justify-center">
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map((itmes) => (
+            <li
+              key={itmes.id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+             <Link onClick={()=>setnav(!nav)} to={itmes.link} smooth duration={500}>{itmes.link}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+
+<div className="w-10 h-10 bg-gray-300 opacity-50 flex items-center justify-center">
         <img
           onClick={() => toggle()}
           src={icon ? lighticon : darkicon}
@@ -72,18 +84,8 @@ const NavBar = () => {
         />
       </div>
 
-      {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen  bg-black">
-          {links.map((items) => (
-            <li
-              key={items.id}
-              className="py-6 text-4xl"
-            >
-              <Link onClick={toggle} to={items.link} smooth duration={500}>{items.link}</Link>
-            </li>
-          ))}
-        </ul>
-      )}
+
+
     </div>
   );
 };
